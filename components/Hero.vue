be20a35c0eb3902db34d607492397b8b3a1effe9
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-08 22:24:58 +0100

    Completes hero area and introduces background assets

--- components/Hero.vue
+++ components/Hero.vue
@@ -19,8 +19,68 @@
       </div>
     </div>
     <div class="flex flex-row justify-center mt-16">
-      <action-button text="Start now for free"></action-button>
+      <action-button text="Start now for free" primary></action-button>
       <action-button class="ml-8" text="Get Premium"></action-button>
+    </div>
+    <div class="flex flex-row justify-center mt-32">
+      <img src="~/assets/img/macbook.png" />
+    </div>
+    <div class="flex flex-row mt-32 justify-center">
+      <div class="flex-col">
+        <div class="flex flex-row justify-center">
+          <div class="flex-col font-serif text-primary2 text-4xl">
+            Open source
+          </div>
+        </div>
+        <div class="flex flex-row justify-center mt-6 max-w-xs">
+          <div class="flex-col font-sans text-primary2 text-2xl text-center">
+            Rotki is a local application. And its basic version is free.
+          </div>
+        </div>
+      </div>
+      <div class="flex-col ml-12">
+        <div class="flex flex-row justify-center">
+          <div class="flex-col font-serif text-primary2 text-4xl">
+            Local app
+          </div>
+        </div>
+        <div class="flex flex-row justify-center mt-6 max-w-xs">
+          <div class="flex-col font-sans text-primary2 text-2xl text-center">
+            Rotki is a local application. And its basic version is also free.
+          </div>
+        </div>
+      </div>
+      <div class="flex-col ml-12">
+        <div class="flex flex-row justify-center">
+          <div class="flex-col font-serif text-primary2 text-4xl">
+            Own your data
+          </div>
+        </div>
+        <div class="flex flex-row justify-center mt-6 max-w-xs">
+          <div class="flex-col font-sans text-primary2 text-2xl text-center">
+            A local application allows you to own your data. You don’t need to
+            give your financial history to a centralized third party.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row mt-12 justify-center align-middle">
+      <div class="flex-col col-span-4">
+        <img src="~/assets/img/accountant.svg" class="h-101" />
+      </div>
+      <div class="flex-col col-auto max-w-xl ml-16 self-center">
+        <div class="flex flex-row">
+          <div class="flex-col text-shade8 text-3xl font-regular align-middle">
+            Rotki offers tracking of all your crypto assets no matter where they
+            are. Either on a blockchain or on one of the supported exchanges.
+          </div>
+        </div>
+        <action-button
+          text="Start now for free"
+          primary
+          class="mt-14"
+        ></action-button>
+      </div>
     </div>
   </div>
 </template>
