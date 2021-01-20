7d4942c49128abc46de5af6f5d3670fd5b64bc21
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-20 20:05:51 +0100

    Implements the basic layout for premium features

--- components/features/Feature.vue
+++ components/features/Feature.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="flex flex-row mt-37">
-    <div class="flex-col col-span-full md:col-span-5 md:col-end-5">
-      <div class="flex flex-row feature-header">
-        <div class="flex-col">
-          <slot name="title"></slot>
+  <div class="container mx-auto">
+    <div class="flex flex-row mt-37">
+      <div class="flex-col col-span-full md:col-span-5 md:col-end-5">
+        <div class="flex flex-row feature-header">
+          <div class="flex-col">
+            <slot name="title"></slot>
+          </div>
         </div>
+        <slot></slot>
       </div>
-      <slot></slot>
     </div>
   </div>
 </template>
