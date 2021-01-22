a27ea2c24439e91259194586d26af03675cb4f05
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-22 17:35:52 +0100

    Introduces footer and makes implements the link functionality

--- components/Links.vue
+++ components/Links.vue
@@ -1,11 +1,27 @@
 <template>
-  <div
-    class="flex flex-row font-sans uppercase text-lg mt-12 justify-center text-primary2"
-  >
-    <div class="flex-col">Features</div>
-    <div class="flex-col ml-8">Documentation</div>
-    <div class="flex-col ml-8">Blog</div>
-    <div class="flex-col ml-8">Contact</div>
+  <div :class="$style.row">
+    <div :class="$style.column">
+      <a href="#features" :class="$style.link"> Features </a>
+    </div>
+    <div :class="$style.column">
+      <a
+        href="https://rotki.readthedocs.io/en/latest/index.html"
+        target="_blank"
+        :class="$style.link"
+      >
+        Documentation
+      </a>
+    </div>
+    <div :class="$style.column">
+      <a href="https://blog.rotki.com/" target="_blank" :class="$style.link">
+        Blog
+      </a>
+    </div>
+    <div :class="$style.column">
+      <a href="mailto:info@rotki.com" target="_blank" :class="$style.link">
+        Contact
+      </a>
+    </div>
   </div>
 </template>
 
@@ -14,3 +30,22 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style module>
+.row {
+  @apply flex flex-row justify-around;
+
+  max-width: 25.1666rem;
+}
+
+.column {
+  @apply flex flex-col font-sans uppercase justify-center text-primary2;
+
+  margin-left: 0.8888rem;
+  margin-right: 0.8888rem;
+}
+
+.link {
+  @apply hover:text-shade8;
+}
+</style>
---
+++ components/footer/PageFooter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="$style.wrapper">
+    <div :class="$style.row">
+      <div :class="$style.logo">
+        <img src="~/assets/img/logo-small.svg" />
+      </div>
+      <div :class="$style.links">
+        <links />
+        <footer-icon-links />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PageFooter',
+})
+</script>
+
+<style module>
+.wrapper {
+  @apply container mx-auto;
+
+  padding-bottom: 3.3333rem;
+}
+
+.row {
+  @apply flex flex-row items-end;
+}
+
+.links {
+  @apply flex flex-row justify-between;
+
+  width: 100%;
+}
+
+.logo {
+  margin-right: 3.3333rem;
+}
+</style>
