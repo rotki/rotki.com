84fa545d0e6a7e5196d9b8fd7b2d51dfe977eab1
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-22 15:00:58 +0100

    Introduces plans section

---
+++ components/plans/AdvancedPlan.vue
@@ -0,0 +1,15 @@
+<template>
+  <plan action="Get advanced plan">
+    <template #title>Advanced</template>
+    Premium users can set watchers for various protocol so that they can be
+    informed via email when certain conditions are met.
+  </plan>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AdvancedPlan',
+})
+</script>
