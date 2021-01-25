abec5a4b2ad1a87e1d714e09bd7b54b50e295b8d
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-25 17:49:16 +0100

    Introduces rotki-og graphic in premium components

--- components/premium/Premium.vue
+++ components/premium/Premium.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="premium">
-    <div class="container mx-auto">
-      <div>
-        <div class="premium-header">Premium Features</div>
+  <div>
+    <div :class="$style['icon-wrapper']">
+      <img :class="$style.top" src="~/assets/img/rotki-og.svg" />
+    </div>
+    <div :class="$style.wrapper">
+      <div :class="$style.main">
+        <div>
+          <div :class="$style.title">Premium Features</div>
+        </div>
+        <div :class="$style.content">
+          <GraphsStatistics />
+          <Transactions />
+          <Defi />
+          <Uniswap />
+          <Eth2Staking />
+          <GetPremium />
+        </div>
       </div>
-      <div class="premium__features">
-        <GraphsStatistics />
-        <Transactions />
-        <Defi />
-        <Uniswap />
-        <Eth2Staking />
-        <GetPremium />
-      </div>
+    </div>
+    <div :class="$style['icon-wrapper']">
+      <img :class="$style.bottom" src="~/assets/img/rotki-og.svg" />
     </div>
   </div>
 </template>
@@ -30,26 +38,70 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped>
-.premium {
+<style module>
+.wrapper {
   @apply bg-primary2 bg-repeat;
 
-  padding-bottom: 4.25rem;
+  padding-bottom: 4.5rem;
 }
 
-.premium-header {
+.main {
+  @apply container mx-auto;
+}
+
+.title {
   @apply text-primary font-serif font-bold;
 
   padding-top: 5.688rem;
   margin-bottom: 3.188rem;
-  font-size: 4.125rem;
-  line-height: 4.75rem;
+  font-size: 3.66666rem;
+  line-height: 4.22222rem;
   align-items: center;
   text-align: center;
   letter-spacing: -0.03em;
 }
 
-.premium__features {
+.content {
   @apply flex-wrap flex-row flex justify-evenly;
+}
+
+.icon-wrapper {
+  position: relative;
+}
+
+.top {
+  position: absolute;
+  top: -17.6rem;
+  left: -3rem;
+  width: 358px;
+  height: 347px;
+  @media only screen and (max-width: 855px) {
+    position: relative;
+    top: 1.7rem;
+  }
+  @media only screen and (min-width: 2200px) {
+    left: 10rem;
+  }
+  @media only screen and (min-width: 3000px) {
+    left: 35rem;
+  }
+}
+
+.bottom {
+  position: absolute;
+  bottom: -3.5rem;
+  right: -10.5rem;
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+  width: 23.94444rem;
+  height: 23.22222rem;
+  @media only screen and (min-width: 2200px) {
+    right: 10rem;
+  }
+  @media only screen and (min-width: 3000px) {
+    right: 35rem;
+  }
+  @media only screen and (max-width: 855px) {
+    position: relative;
+  }
 }
 </style>
