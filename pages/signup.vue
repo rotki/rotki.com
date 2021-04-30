0ff7ffad3b52b526d4b5a9249aab4e774d833777
Konstantinos Paparas <kelsos86@gmail.com>
2021-04-30 17:18:25 +0200

    Implements the basic layout for the signup form

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>Signup</div>
+  <signup-form />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  head: () => {
+    return {
+      title: 'Rotki: Login',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Create a new rotki premium account',
+        },
+      ],
+      htmlAttrs: {
+        class: 'page',
+      },
+      bodyAttrs: {
+        class: 'body',
+      },
+    }
+  },
+})
 </script>
+
+<style lang="scss">
+@import '~assets/css/main';
+</style>
