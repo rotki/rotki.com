5e206dd52147b1edbb4f79d485c0b4270228c8d4
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-12 16:43:53 +0200

    Copies impressum and adjusts css

---
+++ pages/impressum.vue
@@ -0,0 +1,33 @@
+<template>
+  <impressum-content />
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  head: () => {
+    return {
+      title: 'Rotki: Impressum',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Impressum',
+        },
+      ],
+      htmlAttrs: {
+        class: 'page',
+      },
+      bodyAttrs: {
+        class: 'body',
+      },
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+@import '~assets/css/media';
+@import '~assets/css/main';
+</style>
