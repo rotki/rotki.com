76b0fcfa89d2275197fda538754bdfdbcd29cf03
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-18 19:14:53 +0200

    Introduces terms of service page

---
+++ pages/tos.vue
@@ -0,0 +1,32 @@
+<template>
+  <terms-of-service />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'TOS',
+  head: () => {
+    return {
+      title: 'Rotki: Terms of service',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The terms and conditions',
+        },
+      ],
+      htmlAttrs: {
+        class: 'page',
+      },
+      bodyAttrs: {
+        class: 'body',
+      },
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+@import '~assets/css/main';
+</style>
