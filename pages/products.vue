d9af36d52f48aee175a9d913aefa45de8d34627c
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-31 10:58:24 +0200

    Adds basic work on products page

--- pages/products.vue
+++ pages/products.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>Forgot password</div>
+  <shop />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  name: 'Products',
+  head: () => {
+    return {
+      title: 'Rotki: Premium Subscription',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Purchase a premium rotki subscription',
+        },
+      ],
+      htmlAttrs: {
+        class: 'page',
+      },
+      bodyAttrs: {
+        class: 'body',
+      },
+    }
+  },
+})
 </script>
