b05f5430532eff7866db7a5459dcf79c92210bf1
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-31 13:22:01 +0200

    Introduces copyright and legalese links

--- components/footer/PageFooter.vue
+++ components/footer/PageFooter.vue
@@ -9,6 +9,12 @@
         <footer-icon-links />
       </div>
     </div>
+    <div :class="$style.secondRow">
+      <span :class="$style.copyright">
+        © Rotki Solutions GmbH 2018-{{ year }}. All Rights Reserved.
+      </span>
+      <footer-legalese />
+    </div>
   </div>
 </template>
 
@@ -17,6 +23,14 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'PageFooter',
+  data() {
+    return {
+      year: '2021',
+    }
+  },
+  created() {
+    this.year = new Date().getFullYear().toString()
+  },
 })
 </script>
 
@@ -34,14 +48,39 @@ export default Vue.extend({
   @apply flex flex-row items-end;
 }
 
+.secondRow {
+  @apply flex flex-row justify-between flex-wrap;
+
+  margin-top: 48px;
+
+  > * {
+    height: 36px;
+  }
+
+  @include for-size(phone-only) {
+    @apply justify-center;
+  }
+}
+
 .links {
   @apply flex flex-row justify-between flex-wrap;
 
   width: 100%;
 
+  > * {
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+
   @include for-size(phone-only) {
     @apply justify-end;
   }
+}
+
+.copyright {
+  @apply font-sans text-shade11;
+
+  @include text-size(14px, 20px);
 }
 
 .logo {
