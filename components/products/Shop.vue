a58e3e36e750c6abc1d406eb845f67e59a743b3c
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-31 10:58:24 +0200

    Adds basic work on products page

---
+++ components/products/Shop.vue
@@ -0,0 +1,34 @@
+<template>
+  <page>
+    <template #title>Premium Subscription</template>
+    <paragraph small>
+      Rotki is an opensource project. Opensource gives us so many benefits.
+      Owning your data, transparency, auditability and more. But for all its
+      positives, opensource has one major disadvantage and that is how can it be
+      funded?
+    </paragraph>
+
+    <paragraph small>
+      We are taking a unique and direct approach to opensource funding in Rotki.
+      We are offering a premium version which adds extra features on top of the
+      base application. By getting a subscription you are helping us keep the
+      lights open and keep developing opensource. You also get extra benefits
+      for doing so (see below for details).
+    </paragraph>
+
+    <paragraph small>
+      As the project succeeds and grows, so will the benefits provided to you,
+      our premium subscribers. Help us enable you to take ownership of your
+      data! Help us keep portfolio tracking and accounting transparent and
+      opensource! Support Rotki today!
+    </paragraph>
+
+    <heading>Subscriptions</heading>
+  </page>
+</template>
+
+<script>
+export default {
+  name: 'Shop',
+}
+</script>
