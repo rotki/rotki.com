be3696acd802fb4f060763c5d9b3dc5d5e00cb94
Konstantinos Paparas <kelsos86@gmail.com>
2021-05-31 13:22:01 +0200

    Introduces copyright and legalese links

---
+++ components/footer/FooterLegalese.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="$style.links">
+    <external-link same-tab url="/impressum" text="Impressum" />
+    <external-link same-tab url="/privacy-policy" text="Privacy Policy" />
+    <external-link same-tab url="/tos" text="Terms of Service" />
+    <external-link
+      url="https://rotki.readthedocs.io/en/latest/faq.html"
+      text="FAQ"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FooterLegalese',
+})
+</script>
+
+<style module lang="scss">
+@import '~assets/css/main';
+@import '~assets/css/media';
+
+.links {
+  @apply flex flex-row flex-wrap;
+
+  @include for-size(phone-only) {
+    @apply justify-center;
+  }
+
+  > * {
+    margin-left: 8px;
+    margin-right: 8px;
+  }
+}
+</style>
