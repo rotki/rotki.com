a647bc44217328b027cb198842c1543035bf9569
Lefteris Karapetsas <lefteris@refu.co>
2021-06-02 23:52:53 +0200

    Some text fixes on the landing page

--- components/premium/details/PremiumStaking.vue
+++ components/premium/details/PremiumStaking.vue
@@ -2,13 +2,12 @@
   <premium-feature>
     <template #title> Advanced Staking </template>
     <div>
-      <div>See how much ETH was staked per address</div>
-      <div>how much is their expected APR</div>
-      <div>how much ETH you have earned by staking</div>
+      <div>See how much of the token is staked per account.</div>
+      <div>What is the expected APR.</div>
+      <div>How much of the token you have earned by staking across all accounts.</div>
     </div>
     <div class="mt-8">
-      See a list of all your Eth2, and AdEx staking deposits and links to the
-      corresponding transactions.
+      See a list of all your staking events with links to the corresponding transactions.
     </div>
   </premium-feature>
 </template>
