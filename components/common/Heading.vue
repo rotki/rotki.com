b77f4822f970a6cacb24f86fc02bd1edbb1df55d
Konstantinos Paparas <kelsos86@gmail.com>
2021-06-04 15:19:14 +0200

    Adds simple jobs page to the site

--- components/common/Heading.vue
+++ components/common/Heading.vue
@@ -1,5 +1,10 @@
 <template>
-  <div :class="$style.heading">
+  <div
+    :class="{
+      [$style.heading]: !subheading,
+      [$style.subheading]: subheading,
+    }"
+  >
     <slot />
   </div>
 </template>
@@ -9,6 +14,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'Heading',
+  props: {
+    subheading: {
+      required: false,
+      default: false,
+      type: Boolean,
+    },
+  },
 })
 </script>
 
@@ -22,5 +34,13 @@ export default Vue.extend({
   margin-top: 24px;
 
   @include text-size(24px, 32px);
+}
+
+.subheading {
+  @apply font-serif text-primary2 font-medium;
+
+  margin-top: 18px;
+
+  @include text-size(18px, 26px);
 }
 </style>
