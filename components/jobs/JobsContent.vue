1c5b93911b5e945585d4855cb911f768d2a3f31f
Konstantinos Paparas <kelsos86@gmail.com>
2021-06-21 22:44:34 +0200

    Introduces frontend developer job add

--- components/jobs/JobsContent.vue
+++ components/jobs/JobsContent.vue
@@ -1,7 +1,28 @@
 <template>
   <page>
     <template #title> Jobs </template>
-    <backend-developer />
+    <heading>Available Roles</heading>
+    <ul :class="$style.tabs">
+      <li :class="$style.tab">
+        <nuxt-link
+          to="/jobs/backend"
+          :class="$style.link"
+          :active-class="$style.active"
+        >
+          Python Backend Developer
+        </nuxt-link>
+      </li>
+      <li :class="$style.tab">
+        <nuxt-link
+          to="/jobs/frontend"
+          :class="$style.link"
+          :active-class="$style.active"
+        >
+          Vue.js/TypeScript Developer
+        </nuxt-link>
+      </li>
+    </ul>
+    <NuxtChild />
   </page>
 </template>
 
@@ -10,3 +31,25 @@ export default {
   name: 'JobsContent',
 }
 </script>
+
+<style module lang="scss">
+.tabs {
+  @apply flex mb-0 list-none flex-wrap pt-6 pb-6 flex-row;
+}
+
+.tab {
+  @apply -mb-px mr-2 flex-auto text-center font-sans;
+
+  &:last-child {
+    @apply mr-0;
+  }
+}
+
+.link {
+  @apply text-xs font-bold uppercase px-5 py-3 border-primary border rounded block leading-normal text-primary;
+}
+
+.active {
+  @apply text-white bg-primary;
+}
+</style>
---
+++ pages/jobs/frontend.vue
@@ -0,0 +1,11 @@
+<template>
+  <frontend-developer />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Frontend',
+})
+</script>
