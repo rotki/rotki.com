ea941a18e6d266178721b72ec9d477c9f0ebd339
Konstantinos Paparas <kelsos86@gmail.com>
2021-07-23 21:43:34 +0200

    Adds basic validation to the signup form

---
+++ composables/repatcha.ts
@@ -0,0 +1,23 @@
+import { ref } from '@nuxtjs/composition-api'
+
+export const setupRecaptcha = () => {
+  const recaptchaPassed = ref(false)
+  const recaptchaToken = ref('')
+  const onSuccess = (token: string) => {
+    recaptchaToken.value = token
+    recaptchaPassed.value = true
+  }
+  const onExpired = () => {
+    recaptchaPassed.value = false
+  }
+  const onError = () => {
+    recaptchaPassed.value = false
+  }
+  return {
+    recaptchaPassed,
+    recaptchaToken,
+    onSuccess,
+    onExpired,
+    onError,
+  }
+}
