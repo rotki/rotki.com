4ea61e5ad38b102ef462c161efe929d828ddc845
Konstantinos Paparas <kelsos86@gmail.com>
2021-08-17 23:43:00 +0200

    Introduces activation screens

---
+++ pages/activation-invalid.vue
@@ -0,0 +1,31 @@
+<template>
+  <invalid-activation-code />
+</template>
+
+<script lang="ts">
+import { defineComponent, useMeta } from '@nuxtjs/composition-api'
+import { commonAttrs, noIndex } from '~/utils/metadata'
+
+export default defineComponent({
+  name: 'Activated',
+  setup() {
+    useMeta({
+      title: 'Rotki: Account activation failed',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '',
+        },
+        noIndex(),
+      ],
+      ...commonAttrs(),
+    })
+  },
+  head: {},
+})
+</script>
+
+<style lang="scss">
+@import '~assets/css/main';
+</style>
