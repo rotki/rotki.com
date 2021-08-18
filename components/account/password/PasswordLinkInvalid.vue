3d2970f433b30bd45db514f04fecd00869d6f41b
Konstantinos Paparas <kelsos86@gmail.com>
2021-08-18 18:32:27 +0200

    Introduces password recovery flow

---
+++ components/account/password/PasswordLinkInvalid.vue
@@ -0,0 +1,38 @@
+<template>
+  <page>
+    <template #title>Invalid Link</template>
+    <div :class="$style.body">
+      <p :class="$style.header">Invalid password reset link.</p>
+      <p>
+        The link you followed doesn't seem like a valid password reset link.
+      </p>
+    </div>
+  </page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'PasswordLinkInvalid',
+  setup() {},
+})
+</script>
+
+<style lang="scss" module>
+@import '~assets/css/media';
+
+.body {
+  @apply font-sans text-shade11;
+
+  height: 100%;
+
+  @include text-size(18px, 26px);
+}
+
+.header {
+  @apply font-serif text-shade11 pb-2;
+
+  @include text-size(24px, 32px);
+}
+</style>
