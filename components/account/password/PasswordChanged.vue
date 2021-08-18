227b796ce462302d03ae2b05ed7bd2c5def152f8
Konstantinos Paparas <kelsos86@gmail.com>
2021-08-18 18:32:27 +0200

    Introduces password recovery flow

---
+++ components/account/password/PasswordChanged.vue
@@ -0,0 +1,39 @@
+<template>
+  <page>
+    <template #title>Password Change</template>
+    <div :class="$style.body">
+      <p :class="$style.header">Password changed.</p>
+      <div>
+        Your password changed successfully, you can now access your
+        <external-link same-tab text="account" url="/home" />.
+      </div>
+    </div>
+  </page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'PasswordChanged',
+  setup() {},
+})
+</script>
+
+<style lang="scss" module>
+@import '~assets/css/media';
+
+.body {
+  @apply font-sans text-shade11;
+
+  height: 100%;
+
+  @include text-size(18px, 26px);
+}
+
+.header {
+  @apply font-serif text-shade11 pb-2;
+
+  @include text-size(24px, 32px);
+}
+</style>
---
+++ pages/password/invalid-link.vue
@@ -0,0 +1,30 @@
+<template>
+  <password-link-invalid />
+</template>
+
+<script lang="ts">
+import { defineComponent, useMeta } from '@nuxtjs/composition-api'
+import { commonAttrs, noIndex } from '~/utils/metadata'
+
+export default defineComponent({
+  setup() {
+    useMeta({
+      title: 'Rotki: Invalid recovery link',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'This password recovery link is not valid',
+        },
+        noIndex(),
+      ],
+      ...commonAttrs(),
+    })
+  },
+  head: {},
+})
+</script>
+
+<style lang="scss">
+@import '~~/assets/css/main';
+</style>
