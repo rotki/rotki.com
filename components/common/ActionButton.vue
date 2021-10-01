2fa456d8d60b3531b74ce39ddc5da742431a7e6b
Konstantinos Paparas <kelsos86@gmail.com>
2021-10-01 16:45:26 +0200

    Introduces api key regeneration

--- components/common/ActionButton.vue
+++ components/common/ActionButton.vue
@@ -1,10 +1,11 @@
 <template>
   <button
     :class="{
-      [$style.button]: true,
+      [$style.button]: !small,
       [$style.primary]: primary,
       [$style.secondary]: !primary,
       [$style.filled]: filled,
+      [$style.small]: small,
     }"
     :disabled="disabled"
     @click="$emit('click')"
@@ -43,19 +44,40 @@ export default defineComponent({
       required: false,
       default: false,
     },
+    small: {
+      required: false,
+      default: false,
+      type: Boolean,
+    },
   },
 })
 </script>
 
-<style module lang="scss">
+<style lang="scss" module>
 @import '~assets/css/media';
 @import '~assets/css/main';
 
-$button-height: 65px;
-$button-width: 275px;
+.small {
+  @apply font-serif uppercase rounded focus:outline-none;
+
+  $button-height: 48px;
+  $button-width: 220px;
+
+  height: $button-height !important;
+  width: $button-width !important;
+  @include text-size(18px, 26px);
+
+  @include for-size(phone-only) {
+    height: $button-height * $mobile-button-percentage;
+    width: $button-width * $mobile-button-percentage;
+  }
+}
 
 .button {
   @apply font-serif uppercase rounded-full border-2 focus:outline-none focus:ring-1 focus:ring-shade12 focus:ring-opacity-75;
+
+  $button-height: 65px;
+  $button-width: 275px;
 
   height: $button-height;
   width: $button-width;
