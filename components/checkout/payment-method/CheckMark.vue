548f5de7801083bd8b845c6578b3e64e5b0a2945
Konstantinos Paparas <kelsos86@gmail.com>
2021-11-01 13:13:05 +0100

    Extracts check mark and updates plan screen

---
+++ components/checkout/payment-method/CheckMark.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="$style.mark">
+    <check-icon v-if="selected" :class="$style.check" :style="checkStyle" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, toRefs } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'CheckMark',
+  props: {
+    selected: {
+      required: true,
+      type: Boolean,
+    },
+    mt: {
+      required: false,
+      type: String,
+      default: '-18px',
+    },
+  },
+  setup(props) {
+    const { mt } = toRefs(props)
+    return {
+      checkStyle: {
+        'margin-top': mt.value,
+      },
+    }
+  },
+})
+</script>
+<style lang="scss" module>
+.mark {
+  @apply flex flex-row justify-end;
+
+  padding-right: 3px;
+  height: 0;
+  width: 100%;
+}
+
+.check {
+  display: inline;
+  height: 24px;
+  width: 24px;
+}
+</style>
