8097538032f26643d95f34731e48e01e2a2573f2
Konstantinos Paparas <kelsos86@gmail.com>
2021-11-02 18:11:23 +0100

    Extracts title and description components

---
+++ components/checkout/common/CheckoutDescription.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style.description">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'CheckoutDescription',
+})
+</script>
+<style lang="scss" module>
+.description {
+  @apply text-typography;
+
+  line-height: 18px;
+  font-size: 15px;
+  margin-bottom: 56px;
+  letter-spacing: 0;
+}
+</style>
---
+++ components/checkout/common/CheckoutTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style.title">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'CheckoutTitle',
+})
+</script>
+<style lang="scss" module>
+.title {
+  @apply text-typography;
+
+  line-height: 33px;
+  font-size: 28px;
+  letter-spacing: 0;
+  margin-bottom: 16px;
+}
+</style>
