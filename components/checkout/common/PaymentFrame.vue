e837c4868b08cac3e5af17b595365b0ce4a1d841
Konstantinos Paparas <kelsos86@gmail.com>
2021-12-15 18:43:26 +0100

    Adds basis for paypal payments

---
+++ components/checkout/common/PaymentFrame.vue
@@ -0,0 +1,54 @@
+<template>
+  <page wide>
+    <div :class="$style.content">
+      <checkout-title>Payment Details</checkout-title>
+
+      <checkout-description>
+        <div :class="$style.description">
+          <span :class="$style.text">Payments are safely processed with</span>
+          <braintree-icon :class="$style.braintree" />
+        </div>
+      </checkout-description>
+      <loader v-if="loading" :class="$style.loader" />
+      <slot v-else />
+    </div>
+  </page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'PaymentFrame',
+  props: {
+    loading: {
+      required: true,
+      type: Boolean,
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.loader {
+  min-height: 400px;
+}
+
+.braintree {
+  @apply ml-1;
+
+  width: 120px;
+}
+
+.text {
+  @apply pt-0.5;
+}
+
+.description {
+  @apply flex flex-row;
+}
+
+.content {
+  padding: 0;
+}
+</style>
---
+++ components/checkout/pay/PaypalPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <payment-frame :loading="false">
+    <paypal-payment v-if="token" :plan="plan" :token="token" />
+  </payment-frame>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { setupBraintree } from '~/composables/braintree'
+import PaypalPayment from '~/components/checkout/pay/PaypalPayment.vue'
+
+export default defineComponent({
+  name: 'PaypalPage',
+  components: { PaypalPayment },
+  setup() {
+    return setupBraintree()
+  },
+})
+</script>
