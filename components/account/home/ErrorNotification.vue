6eae7201bf5d5f1f79007732b7162cd7e1f8e009
Konstantinos Paparas <kelsos86@gmail.com>
2022-02-16 17:30:43 +0100

    Updates PayPal payment support

---
+++ components/account/home/ErrorNotification.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition name="fade">
+    <notification-message v-if="visible">
+      <div :class="$style.icon">
+        <error-icon />
+      </div>
+      <div :class="$style.errorText">
+        <div :class="$style.errorTitle">
+          <slot name="title" />
+        </div>
+        <div :class="$style.errorDescription">
+          <slot name="description" />
+        </div>
+      </div>
+    </notification-message>
+  </transition>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'ErrorNotification',
+  props: {
+    visible: { required: true, type: Boolean },
+  },
+})
+</script>
+<style lang="scss" module>
+.icon {
+  @apply flex items-center;
+}
+
+.errorText {
+  @apply ml-2;
+}
+
+.errorTitle {
+  @apply font-sans font-bold;
+}
+
+.errorDescription {
+  @apply font-sans text-shade11;
+}
+</style>
---
+++ components/checkout/pay/AcceptRefundPolicy.vue
@@ -0,0 +1,32 @@
+<template>
+  <custom-checkbox id="refund" :value="value" @input="input">
+    <span>
+      I have read and agreed to the
+      <nuxt-link :class="$style.link" target="_blank" to="/refund-policy">
+        Refunds/Cancellation Policy
+      </nuxt-link>
+    </span>
+  </custom-checkbox>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AcceptRefundPolicy',
+  props: {
+    value: { required: true, type: Boolean },
+  },
+  emits: ['input'],
+  setup(_, { emit }) {
+    const input = (value: boolean) => emit('input', value)
+    return { input }
+  },
+})
+</script>
+
+<style module lang="scss">
+.link {
+  @apply text-primary3;
+}
+</style>
