11aa0686ab150078075cefbe1d9d2be034a23811
Joseph Ojoko <entewshi@gmail.com>
2023-03-07 23:43:29 +0100

    add address markdown component

---
+++ components/content/ProseAddress.vue
@@ -0,0 +1,15 @@
+<template>
+  <address :class="css.address">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </address>
+</template>
+
+<script setup lang="ts">
+const css = useCssModule()
+</script>
+
+<style lang="scss" module>
+.address {
+  @apply font-sans text-shade11 mt-6 font-normal not-italic;
+}
+</style>
