e897d8b3a0ac8e1e46b499507ad775589ad7a6b4
Joseph Ojoko <entewshi@gmail.com>
2023-03-09 11:23:32 +0100

    cleanup jobs page, fix page meta data, make it dynamic

--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,36 +1,14 @@
 <script setup lang="ts">
 import { commonAttrs, getMetadata } from '~/utils/metadata';
 
-const metadata = (route: string, baseUrl: string) => {
-  let title = 'jobs | rotki';
-  let description = 'Available roles and positions in the rotki team';
-  let path = '';
+const title = 'jobs | rotki';
+const description = 'Available roles and positions in the rotki team';
 
-  if (route.includes('backend')) {
-    title = 'python backend developer | rotki';
-    description =
-      'We are looking for a backend developer to help us improve Rotki.';
-    path = 'backend';
-  } else if (route.includes('frontend')) {
-    title = 'vue/typescript frontend developer | rotki';
-    description =
-      'We are looking for a passionate Vue.js developer with excellent knowledge of Typescript.';
-    path = 'frontend';
-  }
-
-  return [
-    getMetadata(title, description, `${baseUrl}/jobs/${path}`, baseUrl),
-    title,
-  ] as const;
-};
-
-const route = useRoute();
-const config = useRuntimeConfig();
-const [meta, title] = metadata(route.path, config.baseUrl);
+const { baseUrl } = useRuntimeConfig();
 
 useHead({
   title,
-  meta,
+  meta: getMetadata(title, description, `${baseUrl}/jobs`, baseUrl),
   ...commonAttrs(),
 });
 
