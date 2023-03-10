7cdfbb493dc241b1a1903c35a663d7f8ec86a73c
Joseph Ojoko <entewshi@gmail.com>
2023-03-10 17:28:39 +0100

    extend window interface

---
+++ types/recaptcha.d.ts
@@ -0,0 +1,20 @@
+declare global {
+  interface Window {
+    grecaptcha: {
+      render(
+        el: string | HTMLElement,
+        options: {
+          sitekey: string;
+          callback(token: string): void;
+          'expired-callback'(): void;
+          'error-callback'(): void;
+          theme?: 'light' | 'dark';
+          size?: 'compact' | 'normal';
+        }
+      ): number;
+      reset(id?: number): void;
+    };
+  }
+}
+
+export default {};
