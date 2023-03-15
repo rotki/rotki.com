12d77e48ae654f71dac07ebed9a11b762b968bff
Joseph Ojoko <entewshi@gmail.com>
2023-03-15 16:01:06 +0100

    update tests, test hooks

--- tests/unit/utils/api.nuxt.spec.ts
+++ tests/unit/utils/api.nuxt.spec.ts
@@ -1,17 +1,54 @@
-import { assert, describe, it } from 'vitest';
-import { fetchWithCsrf, sleep } from '~/utils/api';
-import { type ApiResponse } from '~/types';
+import { afterEach, assert, describe, expect, it, vi } from 'vitest';
+import type { Account, ApiResponse } from '~/types';
+import { fetchWithCsrf, setHooks, sleep } from '~/utils/api';
 import { type Country } from '~/composables/countries';
 
-const { BASE_URL } = import.meta.env;
+const logout = vi.fn();
+const refresh = vi.fn();
+setHooks({ logout, refresh });
+
 describe('api utilities', () => {
+  afterEach(() => {
+    logout.mockReset();
+    refresh.mockReset();
+  });
+
   it('fetchWithCsrf: fetch countries runs correctly', async () => {
     const response = await fetchWithCsrf<ApiResponse<Country[]>>(
-      `${BASE_URL}/countries`
+      `/webapi/countries`
     );
 
     assert.isArray(response.result);
+    assert.equal(response.result?.length, 249);
+    expect(refresh).toBeCalled();
+    expect(refresh).toHaveBeenCalledOnce();
   }, 5000);
+
+  it('fetchWithCsrf: fetch account info with 401 error', () => {
+    const response = fetchWithCsrf<ApiResponse<Account>>(`/webapi/account`);
+
+    response.catch((e) => {
+      assert.equal(e.status, 401);
+      expect(logout).toBeCalled();
+      expect(logout).toHaveBeenCalledOnce();
+    });
+  }, 2000);
+
+  it.skip('initCsrf: login and setup csrf successfully', async () => {
+    const response = await fetchWithCsrf<ApiResponse<undefined>>(
+      `/webapi/login/`,
+      {
+        method: 'post',
+        body: {
+          username: '',
+          password: '',
+        },
+      }
+    );
+
+    assert.isString(response.message);
+    assert.isUndefined(response.result);
+  }, 2000);
 
   it('sleep: resolves with the exact timeout', async () => {
     const controller = new AbortController();
