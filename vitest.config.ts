9856991c51a6dda7abcff6b7537f74d2dbe4b3e3
Joseph Ojoko <entewshi@gmail.com>
2023-03-15 16:04:23 +0100

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,14 @@ import { defineVitestConfig } from 'nuxt-vitest/config';
 
 export default defineVitestConfig({
   test: {
-    setupFiles: ['./tests/setup.ts'],
+    globals: true,
+    environment: 'nuxt',
     env: {
-      BASE_URL: process.env.BACKEND_URL || 'https://rotki.com/webapi',
+      BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
+      BACKEND_URL: process.env.BACKEND_URL || 'https://rotki.com',
+      NODE_ENV: 'test',
     },
+    setupFiles: ['./tests/setup.ts'],
   },
   plugins: [],
 });
