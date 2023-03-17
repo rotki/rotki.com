543e7ba82c5fb0485fba0b6460406d8c63d4527b
Joseph Ojoko <entewshi@gmail.com>
2023-03-17 14:47:44 +0100

    add replacement for startup plugin

---
+++ plugins/startup.ts
@@ -0,0 +1,17 @@
+import { useMainStore } from '~/store';
+import { SESSION_COOKIE } from '~/utils/api';
+
+export default defineNuxtPlugin(async () => {
+  const { name } = useRoute();
+  if (name === 'health') {
+    return;
+  }
+
+  const sessionId = useCookie(SESSION_COOKIE).value;
+
+  if (sessionId) {
+    const { getAccount } = useMainStore();
+
+    return await getAccount();
+  }
+});
