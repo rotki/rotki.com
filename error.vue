e8db966a17e081d1fd0d2956270b794b64033fd8
Joseph Ojoko <entewshi@gmail.com>
2023-04-12 15:28:29 +0100

    add error renderer page, so it renders like every other page

---
+++ error.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { get } from '@vueuse/core';
+import { commonAttrs, noIndex } from '~/utils/metadata';
+
+const error = useError();
+const { t } = useI18n();
+const css = useCssModule();
+
+const pageTitle = computed(() => get(error)?.message);
+const title = computed(() => `${get(pageTitle)} | rotki`);
+
+const handleError = () => clearError({ redirect: '/' });
+
+useHead(() => ({
+  title,
+  meta: [noIndex()],
+  ...commonAttrs(),
+}));
+</script>
+
+<template>
+  <PageContainer>
+    <div v-if="error" :class="css.wrapper">
+      <h1 :class="css.title">{{ error.statusCode }}</h1>
+      <p :class="css.subtitle">{{ pageTitle }}</p>
+      <ActionButton
+        :text="t('actions.go_back_home')"
+        primary
+        small
+        @click="handleError"
+      />
+    </div>
+  </PageContainer>
+</template>
+
+<style module lang="scss">
+.wrapper {
+  @apply text-center my-24;
+}
+.title {
+  @apply block font-serif text-red-500 font-bold text-6xl;
+}
+
+.subtitle {
+  @apply block font-sans font-light text-primary2 my-8 text-3xl;
+}
+</style>
