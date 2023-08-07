17421692503d50042140e4f7011269eeb3ab324f
lukicenturi <lukicenturi@gmail.com>
2023-08-07 17:33:50 +0700

    style: adjust footer in mobile screen

--- components/footer/PageFooter.vue
+++ components/footer/PageFooter.vue
@@ -16,33 +16,34 @@ const year = new Date().getFullYear().toString();
 <template>
   <div>
     <div class="container">
-      <div
-        v-if="landing"
-        class="space-y-4 lg:space-y-8 mt-8 mb-4 lg:mt-16 lg:mb-10"
-      >
-        <div class="flex flex-col lg:flex-row justify-between items-center">
-          <div class="hidden lg:block">
-            <RuiLogo text />
+      <div v-if="landing" class="space-y-8 mt-8 lg:mt-16 mb-10">
+        <div class="flex justify-between items-center">
+          <div>
+            <RuiLogo class="!h-8 md:h-12" text />
           </div>
-          <NuxtLink v-if="false" to="/download">
+          <NuxtLink to="/download">
             <RuiButton color="primary" variant="text" size="lg">
               {{ t('page_footer.download') }}
             </RuiButton>
           </NuxtLink>
         </div>
-        <div class="flex flex-col xl:flex-row justify-between items-center">
-          <div class="hidden lg:block">
-            <NavigationMenu />
-          </div>
-          <FooterLegalese />
+        <div
+          class="flex md:flex-col xl:flex-row justify-between items-start md:items-center"
+        >
+          <NavigationMenu
+            class="grow xl:grow-0 flex-col md:flex-row gap-y-3 !gap-x-4"
+          />
+          <FooterLegalese
+            class="grow xl:grow-0 mt-0 md:mt-4 xl:mt-0 flex-col md:flex-row gap-y-3"
+          />
         </div>
       </div>
       <div class="border-t border-rui-grey-100" />
       <div
-        class="flex flex-col-reverse lg:flex-row justify-between items-center pt-4 pb-6 lg:pt-8 lg:pb-12"
+        class="flex flex-col-reverse lg:flex-row justify-between items-start md:items-center pt-8 pb-12"
       >
         <div
-          class="flex flex-wrap justify-center space-x-1 text-rui-text-secondary mt-2 lg:mt-0"
+          class="flex flex-wrap space-x-1 text-rui-text-secondary mt-6 lg:mt-0"
         >
           <div>&copy; Rotki Solutions GmbH 2018-{{ year }}.</div>
           <div>All Rights Reserved.</div>
