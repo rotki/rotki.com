37bf18e2f2e50a989a9126e7f5b80f46242ea46e
Konstantinos Paparas <kelsos86@gmail.com>
2023-08-11 16:02:20 +0200

    test: adjusts mocks and expectations

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -3,6 +3,18 @@ import { rest } from 'msw';
 const { BACKEND_URL } = import.meta.env;
 
 export const handlers = [
+  rest.get(`${BACKEND_URL}/webapi/csrf`, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        message: 'CSRF cookie set',
+      }),
+      ctx.cookie('csrftoken', '1234'),
+    ),
+  ),
+  rest.post(`${BACKEND_URL}/webapi/login`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json({ message: 'success' })),
+  ),
   rest.get(`${BACKEND_URL}/webapi/countries`, (req, res, ctx) =>
     res(
       ctx.status(200),
