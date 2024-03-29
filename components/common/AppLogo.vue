2842b40e24d230e853828bb93e554b7ba1127850
Joseph Ojoko <entewshi@gmail.com>
2024-03-29 16:11:09 +0100

    fix: hide server rendered image

--- components/common/AppLogo.vue
+++ components/common/AppLogo.vue
@@ -21,9 +21,11 @@ const attrs = useAttrs();
       :text="text"
       :custom-src="src"
       v-bind="attrs"
+      class="transition delay-1000"
     />
     <template #fallback>
       <RuiLogo
+        class="opacity-0 invisible transition delay-1000"
         :text="text"
         v-bind="attrs"
       />
