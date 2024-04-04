b465b12296460361e39089aa838ceb2b0bfdb36a
Luki Centuri <lukicenturi@gmail.com>
2024-04-04 22:31:41 +0700

    feat(frontend): add resend email verification button (#205)

--- utils/text.ts
+++ utils/text.ts
@@ -13,3 +13,17 @@ export function toTitleCase(string: string): string {
     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(' ');
 }
+
+export function formatSeconds(number: number): { minutes?: number; seconds?: number } {
+  const rounded = Math.floor(number);
+  const minutes = Math.floor(rounded / 60);
+  const seconds = rounded % 60;
+
+  const result: { minutes?: number; seconds?: number } = {};
+  if (minutes > 0)
+    result.minutes = minutes;
+  if (seconds > 0)
+    result.seconds = seconds;
+
+  return result;
+}
