123e3915c87d0f9781e43895cdc57993b26143de
lukicenturi <lukicenturi@gmail.com>
2024-09-04 22:24:01 +0700

    fix: adjust trailing slash for API call in frontend

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -3,32 +3,32 @@ import { HttpResponse, http } from 'msw';
 const { BACKEND_URL } = import.meta.env;
 
 export const handlers = [
-  http.options(`${BACKEND_URL}/webapi/csrf`, () => HttpResponse.json({}, {
+  http.options(`${BACKEND_URL}/webapi/csrf/`, () => HttpResponse.json({}, {
     headers: {
       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
       'Access-Control-Allow-Origin': '*',
     },
   })),
-  http.options(`${BACKEND_URL}/webapi/countries`, () => HttpResponse.json({}, {
+  http.options(`${BACKEND_URL}/webapi/countries/`, () => HttpResponse.json({}, {
     headers: {
       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
       'Access-Control-Allow-Origin': '*',
     },
   })),
-  http.options(`${BACKEND_URL}/webapi/login`, () => HttpResponse.json({}, {
+  http.options(`${BACKEND_URL}/webapi/login/`, () => HttpResponse.json({}, {
     headers: {
       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
       'Access-Control-Allow-Origin': '*',
 
     },
   })),
-  http.options(`${BACKEND_URL}/webapi/account`, () => HttpResponse.json({}, {
+  http.options(`${BACKEND_URL}/webapi/account/`, () => HttpResponse.json({}, {
     headers: {
       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
       'Access-Control-Allow-Origin': '*',
     },
   })),
-  http.get(`${BACKEND_URL}/webapi/csrf`, () =>
+  http.get(`${BACKEND_URL}/webapi/csrf/`, () =>
     HttpResponse.json(
       {
         message: 'CSRF cookie set',
@@ -39,16 +39,16 @@ export const handlers = [
         },
       },
     )),
-  http.post(`${BACKEND_URL}/webapi/login`, () =>
+  http.post(`${BACKEND_URL}/webapi/login/`, () =>
     HttpResponse.json({ message: 'success' })),
-  http.get(`${BACKEND_URL}/webapi/countries`, () =>
+  http.get(`${BACKEND_URL}/webapi/countries/`, () =>
     HttpResponse.json({
       message: 'it works!',
       result: [{ code: 'CT', name: 'Country' }],
     })),
-  http.get(`${BACKEND_URL}/webapi/countries`, () =>
+  http.get(`${BACKEND_URL}/webapi/countries/`, () =>
     HttpResponse.json({ message: 'it works!!', result: [] })),
-  http.get(`${BACKEND_URL}/webapi/account`, () =>
+  http.get(`${BACKEND_URL}/webapi/account/`, () =>
     HttpResponse.json(
       { message: 'fail', result: [] },
       {
