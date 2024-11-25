2fd0cb121181eebc4887e546d132a38f65d4b650
Konstantinos Paparas <kelsos86@gmail.com>
2024-11-25 12:25:36 +0100

    refactor: enable chain icons for testing

--- components/checkout/pay/CryptoChainIcon.vue
+++ components/checkout/pay/CryptoChainIcon.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   chain: string;
 }>();
 
 const pending = ref(true);
 const error = ref(false);
+
+const { public: { testing } } = useRuntimeConfig();
+
+const chainName = computed<string>(() => {
+  if (!testing)
+    return props.chain;
+
+  return props.chain.replace(/sepolia/g, '').trim();
+});
 </script>
 
 <template>
@@ -18,7 +27,7 @@ const error = ref(false);
     <img
       class="w-full h-full"
       :class="{ hidden: pending || error }"
-      :src="`/img/chains/${chain}.svg`"
+      :src="`/img/chains/${chainName}.svg`"
       @loadstart="pending = true"
       @load="pending = false"
       @error="
