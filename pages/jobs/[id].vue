e116f6f290770bee6ee2507f124ba805c947c76c
Konstantinos Paparas <kelsos86@gmail.com>
2025-02-12 16:09:04 +0100

    fix: change the message for not open roles
    Not open roles will now give a role unavailable message
    instead of 404

--- pages/jobs/[id].vue
+++ pages/jobs/[id].vue
@@ -5,20 +5,28 @@ import { commonAttrs, getMetadata } from '~/utils/metadata';
 const { public: { baseUrl } } = useRuntimeConfig();
 const { path } = useRoute();
 const { fallbackToLocalOnError } = useRemoteOrLocal();
+const { t } = useI18n();
 
 const { data: job } = await useAsyncData(path, () => fallbackToLocalOnError(
   async () => await queryCollection('jobsRemote').path(path).first(),
   async () => await queryCollection('jobsLocal').path(path).first(),
 ));
 
-if (!get(job)?.open) {
+if (!isDefined(job)) {
   showError({ message: `Page not found: ${path}`, statusCode: 404 });
 }
 else {
+  const { title, description, open } = get(job);
+
+  const meta = {
+    title: open ? title : `${title} (${t('jobs.role_unavailable.title')})`,
+    description: open ? description : t('jobs.role_unavailable.description', { title }),
+  };
+
   useHead({
     meta: getMetadata(
-      get(job)?.title ?? '',
-      get(job)?.description ?? '',
+      meta.title,
+      meta.description,
       `${baseUrl}${path}`,
       baseUrl,
     ),
@@ -39,6 +47,20 @@ definePageMeta({
     <template #title>
       {{ job.title }}
     </template>
-    <JobDetail :data="job" />
+    <JobDetail
+      v-if="job.open"
+      :data="job"
+    />
+    <div class="py-8 lg:py-20">
+      <div class="container flex flex-col lg:flex-row">
+        <div>
+          <div class="text-h5 font-medium mb-4">
+            {{ t("jobs.role_unavailable.title") }}
+          </div>
+
+          {{ t("jobs.role_unavailable.description", { title: job.title }) }}
+        </div>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
