d6bcabc4aa2b952740aada2027a56a16d5cda56e
Konstantinos Paparas <kelsos86@gmail.com>
2025-03-21 13:36:51 +0100

    chore: update happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineVitestConfig({
   plugins: [],
   test: {
     env: {
-      BACKEND_URL: process.env.NUXT_PUBLIC_BACKEND_URL || 'https://rotki.com',
+      BACKEND_URL: process.env.NUXT_PUBLIC_BACKEND_URL || 'http://localhost:3000',
       BASE_URL: process.env.NUXT_PUBLIC_BASE_URL || 'http://localhost:3000',
       NODE_ENV: 'test',
     },
