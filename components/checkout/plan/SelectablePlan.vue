996ce7f5f11104935f0a361f106a9498c86c3ece
lukicenturi <lukicenturi@gmail.com>
2025-06-27 13:38:41 +0700

    fix: add check if monthly or yearly plan is missing

--- components/checkout/plan/SelectablePlan.vue
+++ components/checkout/plan/SelectablePlan.vue
@@ -2,6 +2,7 @@
 import type { AvailablePlan } from '~/types';
 import { get } from '@vueuse/core';
 import { PricingPeriod } from '~/types/tiers';
+import { formatCurrency } from '~/utils/text';
 
 const props = withDefaults(
   defineProps<{
@@ -16,25 +17,35 @@ const props = withDefaults(
   },
 );
 
-const emit = defineEmits<{ (e: 'click'): void }>();
+const emit = defineEmits<{
+  click: [];
+  clear: [];
+}>();
 
 const { t } = useI18n({ useScope: 'global' });
 
-const { plan, period } = toRefs(props);
+const { plan, period, selected } = toRefs(props);
 
-const isMostPopular = computed(() => get(plan).tierName === 'next'); // TODO: get this data from backend
-
-const price = computed(() => {
+const price = computed<string | undefined>(() => {
   const { monthlyPlan, yearlyPlan } = get(plan);
   const periodVal = get(period);
 
   const targetPlan = periodVal === PricingPeriod.YEARLY ? yearlyPlan : monthlyPlan;
-  return targetPlan ? parseFloat(targetPlan.price).toFixed(2) : '0.00';
+  if (!targetPlan)
+    return undefined;
+  return formatCurrency(parseFloat(targetPlan.price));
+});
+
+watch(price, (price) => {
+  if (!price && get(selected)) {
+    emit('clear');
+  }
 });
 </script>
 
 <template>
   <div
+    v-if="price"
     class="rounded-xl border border-default p-4 cursor-pointer"
     :class="{
       '!border-rui-primary': selected,
@@ -43,7 +54,7 @@ const price = computed(() => {
     @click="emit('click')"
   >
     <RuiChip
-      v-if="isMostPopular"
+      v-if="plan.isMostPopular"
       color="primary"
       class="mb-2 !min-h-4"
       :class="{
