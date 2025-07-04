35cbf0f84845c3ac6915c064c261a7a799211321
Luki Centuri <lukicenturi@gmail.com>
2025-07-04 23:11:03 +0700

    chore: move device page (#315)

---
+++ pages/home/devices.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'account',
+  middleware: ['maintenance', 'authentication'],
+});
+</script>
+
+<template>
+  <UserDevicesTable />
+</template>
