81fcf820138a39ab812e6cba8effe763f2b68f81
lukicenturi <lukicenturi@gmail.com>
2025-08-30 10:18:31 +0700

    fix: small improvement for empty leaderboard, and image cache

--- server/utils/cache.ts
+++ server/utils/cache.ts
@@ -3,7 +3,7 @@
  */
 export const CACHE_TTL = {
   CONFIG: 5 * 60, // 5 minutes
-  IMAGE: 24 * 60 * 60, // 24 hours
+  IMAGE: 7 * 24 * 60 * 60, // 7 days
   METADATA: 60 * 60, // 1 hour
   RELEASE_ID: 10 * 60, // 10 minutes
   TIER_DATA: 5 * 60, // 5 minutes
