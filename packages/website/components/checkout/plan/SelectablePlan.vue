ae8c5b5146bf0c914db8a49f37899e9f203d0a8a
Luki Centuri <lukicenturi@gmail.com>
2025-10-22 21:52:13 +0700

    style: adjust styling

--- packages/website/components/checkout/plan/SelectablePlan.vue
+++ packages/website/components/checkout/plan/SelectablePlan.vue
@@ -40,39 +40,35 @@ function click() {
 
 <template>
   <div
-    class="flex flex-col items-center min-w-[14.5rem] xl:min-w-[13rem] 2xl:min-w-[13.5rem] w-full h-full px-6 py-8 border border-solid rounded-lg cursor-pointer bg-white hover:bg-rui-primary/[0.01] border-black/[0.12] relative"
+    class="min-w-[14.5rem] xl:min-w-[13rem] 2xl:min-w-[13.5rem] w-full h-full px-6 py-6 border border-solid rounded-lg cursor-pointer bg-white hover:bg-rui-primary/[0.01] border-black/[0.12] relative"
     :class="{ 'border-rui-primary': selected }"
     @click="click()"
   >
-    <div class="flex items-center h-0 justify-center relative w-full">
+    <div class="flex justify-between items-start w-full h-12">
       <RuiChip
         v-if="popular"
-        class="-top-[2.9rem] absolute"
         color="primary"
         size="sm"
+        content-class="text-xs"
       >
         {{ t('home.plans.most_popular') }}
       </RuiChip>
+      <RuiChip
+        v-if="plan.discount"
+        size="sm"
+        class="!bg-green-400 font-bold text-xs"
+      >
+        {{ plan.discount }}% OFF
+      </RuiChip>
     </div>
 
-    <div class="w-full flex justify-between items-center my-1 h-8">
-      <div class="h-full flex items-center">
-        <div
-          v-if="plan.discount"
-          class="px-2.5 py-1 text-[0.625rem] font-bold uppercase tracking-wider bg-green-400 text-black rounded-full whitespace-nowrap"
-        >
-          {{ plan.discount }}% OFF
-        </div>
-      </div>
-      <CheckMark :selected="selected" />
-    </div>
-    <div class="text-h5 text-rui-text mb-6">
+    <div class="text-h5 text-rui-text mb-3">
       {{ name }}
     </div>
 
     <div
       v-if="plan.originalPrice"
-      class="text-base text-rui-text-secondary line-through font-normal opacity-70 h-6 flex items-center justify-center mb-1"
+      class="text-base text-rui-text-secondary line-through font-normal opacity-70 h-6 mb-1"
     >
       {{ plan.originalPrice }}€
     </div>
@@ -85,11 +81,11 @@ function click() {
       {{ totalPrice }}€
     </div>
 
-    <div class="text-sm text-rui-text-secondary mt-3 mb-6 font-normal flex flex-col items-center justify-start h-[2.75rem]">
+    <div class="text-sm text-rui-text-secondary mt-3 mb-4 font-normal flex flex-col justify-start h-[2.75rem]">
       <div>{{ price }}€/{{ t('home.plans.per_month') }}</div>
       <div
         v-if="plan.freeMonths"
-        class="text-xs"
+        class="text-xs text-red-600"
       >
         {{ t('home.plans.saving', { months: plan.freeMonths }) }}
       </div>
@@ -98,7 +94,19 @@ function click() {
       :color="selected ? 'primary' : undefined"
       class="w-full"
     >
-      {{ t('home.plans.choose') }}
+      <template
+        v-if="selected"
+        #prepend
+      >
+        <div class="rounded-full size-4 bg-white flex items-center justify-center">
+          <RuiIcon
+            name="lu-check"
+            color="primary"
+            size="12"
+          />
+        </div>
+      </template>
+      {{ selected ? t('home.plans.plan_selected') : t('home.plans.choose') }}
     </RuiButton>
   </div>
 </template>
