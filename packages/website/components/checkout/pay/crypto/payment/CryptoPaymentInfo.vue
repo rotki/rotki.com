4ac08d25f7ac231b4ccf2e079b677738158335c4
Konstantinos Paparas <kelsos86@gmail.com>
2025-10-23 17:37:47 +0200

    feat: introduce two column mode for crypto payment request

--- packages/website/components/checkout/pay/crypto/payment/CryptoPaymentInfo.vue
+++ packages/website/components/checkout/pay/crypto/payment/CryptoPaymentInfo.vue
@@ -1,30 +1,55 @@
 <script setup lang="ts">
+import { get, set } from '@vueuse/core';
+
 const { t } = useI18n({ useScope: 'global' });
+
+const expanded = ref<boolean>(true);
 
 const notes = computed<string[]>(() => [
   t('home.plans.tiers.step_3.notes.line_1'),
   t('home.plans.tiers.step_3.notes.line_2'),
   t('home.plans.tiers.step_3.notes.line_3'),
 ]);
+
+function toggle(): void {
+  set(expanded, !get(expanded));
+}
 </script>
 
 <template>
-  <div class="mt-8 text-body-2">
-    <div
-      v-for="(note, i) in notes"
-      :key="i"
-      class="flex flex-row mt-5 gap-2"
+  <div class="text-body-2">
+    <button
+      type="button"
+      class="flex items-center gap-2 text-rui-primary hover:text-rui-primary-darker transition-colors w-full"
+      @click="toggle()"
     >
-      <div>
-        <RuiChip
-          color="primary"
-          size="sm"
-        >
-          <span class="h-6 flex items-center justify-center">{{ i + 1 }}</span>
-        </RuiChip>
-      </div>
-      <div>
-        {{ note }}
+      <RuiIcon
+        :name="expanded ? 'lu-chevron-down' : 'lu-chevron-right'"
+        size="20"
+      />
+      <span class="font-medium">{{ t('home.plans.tiers.step_3.how_crypto_works') }}</span>
+    </button>
+
+    <div
+      v-if="expanded"
+      class="mt-4"
+    >
+      <div
+        v-for="(note, i) in notes"
+        :key="i"
+        class="flex flex-row mt-4 gap-2"
+      >
+        <div>
+          <RuiChip
+            color="primary"
+            size="sm"
+          >
+            <span class="h-6 flex items-center justify-center">{{ i + 1 }}</span>
+          </RuiChip>
+        </div>
+        <div>
+          {{ note }}
+        </div>
       </div>
     </div>
   </div>
