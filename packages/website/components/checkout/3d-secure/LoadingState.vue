f036ea3389736afe03283bd877e474f191932c3f
Konstantinos Paparas <kelsos86@gmail.com>
2025-10-27 10:40:00 +0100

    style: adjust 3d-secure to use two columns on desktop

--- packages/website/components/checkout/3d-secure/LoadingState.vue
+++ packages/website/components/checkout/3d-secure/LoadingState.vue
@@ -23,64 +23,68 @@ const loadingMessages = computed<Record<ThreeDSecureState, string>>(() => ({
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <!-- Payment Information -->
-    <PaymentInfoCard
-      v-if="paymentInfo"
-      :payment-info="paymentInfo"
-      class="mb-2 -mt-2"
-    />
-
-    <!-- Loading indicator for non-challenge states -->
-    <div
-      v-if="!challengeVisible"
-      class="flex flex-col items-center"
-    >
-      <RuiProgress
-        variant="indeterminate"
-        size="48"
-        circular
-        color="primary"
-        class="my-4"
+  <div class="flex flex-col gap-8 xl:grid xl:grid-cols-[1.5fr_1fr] xl:gap-12 xl:items-start">
+    <!-- Payment Information - First on mobile, Right on desktop -->
+    <aside class="w-full xl:sticky xl:top-8 xl:self-start xl:order-2">
+      <PaymentInfoCard
+        v-if="paymentInfo"
+        :payment-info="paymentInfo"
       />
+    </aside>
 
-      <p class="text-rui-text-secondary text-center max-w-md">
-        {{ loadingMessages[state] }}
-      </p>
-
-      <!-- Security notice -->
-      <div class="mt-6 text-xs text-rui-text-secondary text-center max-w-sm">
-        <RuiIcon
-          name="lu-lock"
-          size="12"
-          class="inline mr-1"
-        />
-        {{ t('subscription.3d_secure.security_notice') }}
-      </div>
-    </div>
-
-    <!-- Challenge iframe container - Always in DOM, visibility controlled -->
-    <div
-      v-show="challengeVisible"
-      class="w-full max-w-2xl mx-auto flex flex-col items-center"
-    >
-      <div class="my-3">
-        <RuiAlert
-          type="info"
-          variant="outlined"
-          icon="lu-shield-check"
-        >
-          <span class="font-medium">
-            {{ t('subscription.3d_secure.challenge_instructions') }}
-          </span>
-        </RuiAlert>
-      </div>
-
-      <!-- iframe container - Always in DOM -->
+    <!-- 3D Secure Challenge - Second on mobile, Left on desktop -->
+    <div class="flex flex-col min-w-0 xl:order-1">
+      <!-- Loading indicator for non-challenge states -->
       <div
-        id="threeds-iframe-container"
-        class="max-w-full"
-      />
+        v-if="!challengeVisible"
+        class="flex flex-col items-center justify-center min-h-[300px]"
+      >
+        <RuiProgress
+          variant="indeterminate"
+          size="48"
+          circular
+          color="primary"
+          class="my-4"
+        />
+
+        <p class="text-rui-text-secondary text-center max-w-md">
+          {{ loadingMessages[state] }}
+        </p>
+
+        <!-- Security notice -->
+        <div class="mt-6 text-xs text-rui-text-secondary text-center max-w-sm">
+          <RuiIcon
+            name="lu-lock"
+            size="12"
+            class="inline mr-1"
+          />
+          {{ t('subscription.3d_secure.security_notice') }}
+        </div>
+      </div>
+
+      <!-- Challenge iframe container - Always in DOM, visibility controlled -->
+      <div
+        v-show="challengeVisible"
+        class="w-full flex flex-col"
+      >
+        <div class="mb-4">
+          <RuiAlert
+            type="info"
+            variant="outlined"
+            icon="lu-shield-check"
+          >
+            <span class="font-medium">
+              {{ t('subscription.3d_secure.challenge_instructions') }}
+            </span>
+          </RuiAlert>
+        </div>
+
+        <!-- iframe container - Always in DOM -->
+        <div
+          id="threeds-iframe-container"
+          class="w-full"
+        />
+      </div>
     </div>
   </div>
 </template>
