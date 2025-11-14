6b2d35a770c64a71768d89d98b75eb3afb0241f9
Konstantinos Paparas <kelsos86@gmail.com>
2025-11-14 17:25:34 +0100

    refactor: split pricing components and function

--- packages/website/components/pricings/PricingHeading.vue
+++ packages/website/components/pricings/PricingHeading.vue
@@ -3,17 +3,12 @@ const { t } = useI18n({ useScope: 'global' });
 </script>
 
 <template>
-  <div class="container">
-    <div class="py-10 pb-20 flex flex-col gap-4">
-      <div class="text-rui-light-primary text-h6 font-medium">
-        {{ t('pricing.header.heading') }}
-      </div>
-      <h4 class="text-h4 !font-bold">
-        {{ t('home.plans.tiers.step_1.title') }}
-      </h4>
-      <div class="max-w-[768px] text-body-1 text-rui-text-secondary">
-        {{ t('home.plans.tiers.step_1.description') }}
-      </div>
-    </div>
+  <div class="mb-6">
+    <CheckoutTitle>
+      {{ t('home.plans.tiers.step_1.title') }}
+    </CheckoutTitle>
+    <CheckoutDescription>
+      {{ t('home.plans.tiers.step_1.description') }}
+    </CheckoutDescription>
   </div>
 </template>
