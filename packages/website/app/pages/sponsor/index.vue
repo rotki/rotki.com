3d4cf87867a7e223666f20f37b47e6de59024a17
Konstantinos Paparas <kelsos86@gmail.com>
2026-01-23 15:49:18 +0100

    fix: add redirect from /sponsor to /sponsor/mint with 301 status

---
+++ packages/website/app/pages/sponsor/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+  middleware: 'sponsorship',
+});
+
+await navigateTo('/sponsor/mint', { redirectCode: 301 });
+</script>
+
+<template>
+  <div />
+</template>
